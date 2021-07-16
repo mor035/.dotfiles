33b6531fdbb2a64b661242a228e3efa9005bcbed
mor035 <mchg@pm.me>
2021-07-16 22:36:24 +0200

    added sublime-text-4

--- packageInstaller.sh
+++ packageInstaller.sh
@@ -3,8 +3,9 @@ pacman -Syu --noconfirm
 yes | LC_ALL=en_US.UTF-8 pacman -S \
 	arandr blender capitaine-cursors chromium cmatrix discord ffmpeg firefox gimp gnome-keyring gparted gpick jupyter \
 	jupyterlab libreoffice-still lightdm-settings neofetch okular pavucontrol pcmanfm pulseaudio qbittorrent r ranger git \
-	rsync speedcrunch speedtest-cli sqlitebrowser steam-manjaro stow sublime-text texlive-most texlive-lang texlive-bibtexextra \
+	rsync speedcrunch speedtest-cli sqlitebrowser steam-manjaro stow texlive-most texlive-lang texlive-bibtexextra \
 	biber unison unzip viewnior vim vlc wine xournalpp inkscape yay zathura zathura-djvu zathura-pdf-mupdf zathura-ps zip zsh 
 yay -S --noconfirm \
 	lightdm-mini-greeter openrefine rstudio-desktop-bin spicetify-cli spotify teamviewer timeset timeset-gui marvin\
-	trezor-suite-appimage trezor-udev zoom zotero cava cointop anaconda la-capitaine-icon-theme ganttproject balena-etcher
+	trezor-suite-appimage trezor-udev zoom zotero cava cointop anaconda la-capitaine-icon-theme ganttproject balena-etcher\ 
+	sublime-text-4
