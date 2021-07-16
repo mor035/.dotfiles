fd465b644f1811bac58df19e8330e08b0495aa5f
mor035 <mchg@pm.me>
2021-07-16 22:33:22 +0200

    rm sublime-text

--- packageInstaller.sh
+++ packageInstaller.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg &&\
-	sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
 pacman -Syu --noconfirm
 yes | LC_ALL=en_US.UTF-8 pacman -S \
 	arandr blender capitaine-cursors chromium cmatrix discord ffmpeg firefox gimp gnome-keyring gparted gpick jupyter \
